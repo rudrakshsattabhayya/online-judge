a472a0acf3e92d090e89569d13bf0ec418f6a548
Rudraksh Sattabhayya <rudrakshsattabhayya@gmail.com>
2022-07-02 19:32:14 +0530

    updated models

--- oj_backend/prob_app/models.py
+++ oj_backend/prob_app/models.py
@@ -4,15 +4,15 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=40)
+    tag_name = models.CharField(max_length=40, default="default")
 
     def __str__(self):
         return self.tag_name
 
 
 class ProblemDescription(models.Model):
-    prob_statment = models.CharField(max_length=1000, blank=True)
-    prob_solution = models.CharField(max_length=10000, blank=True)
+    prob_statment = models.TextField(max_length=1000, blank=True)
+    prob_solution = models.TextField(max_length=10000, blank=True)
 
     def __str__(self):
         return self.prob_statment[:40]+"..."
@@ -27,12 +27,12 @@ class TestCase(models.Model):
 
 class Problem(models.Model):
     prob_name = models.CharField(max_length=100)
-    difficulty = models.CharField(max_length=10)
+    difficulty = models.IntegerField(default=800)
     accepted_submissions = models.IntegerField(default=0)
     totalsubmissions = models.IntegerField(default=0)
     test_cases = models.OneToOneField(TestCase, on_delete=models.CASCADE, null=True)
     problem_desc = models.OneToOneField(ProblemDescription, on_delete=models.CASCADE, null=True)
-    # tags = models.ForeignKey(Tag, on_delete=models.PROTECT, null=True)
+    tags = models.ManyToManyField(Tag, related_name="tags", default="default")
 
     def __str__(self):
         return self.prob_name
